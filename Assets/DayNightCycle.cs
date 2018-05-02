fab6dddbd30d8195cf1f3c6932e5230669c5b5a4
Kevin Menendez Soto <kev.ms93@gmail.com>
2018-05-02 21:54:28 +0200

    First version of the Asset

---
+++ Assets/DayNightCycle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+    [Header("Sun/Moon properties")]
+    public float distanceSun = 0f;
+    public float rotationSun = 90f;
+    public float timeRotation = 5f;
+    public bool realTime = false;
+
+    private GameObject _sun;
+    private GameObject _moon;
+
+    void Start () {
+		if(distanceSun == 0f)
+        {
+            distanceSun = 500f;
+        }
+
+        _sun = transform.GetChild(0).gameObject;
+        _moon = transform.GetChild(1).gameObject;
+
+        _sun.transform.position = new Vector3(0, distanceSun, 0);
+        _moon.transform.position = new Vector3(0, distanceSun * -1, 0);
+
+        _sun.transform.rotation = new Quaternion(rotationSun, 0, 0, _sun.transform.rotation.w);
+        _moon.transform.rotation = new Quaternion(rotationSun * -1, 0, 0, _moon.transform.rotation.w);
+    }
+	
+	void Update () {
+        RotateElement(_sun);
+        RotateElement(_moon);
+    }
+
+    private void RotateElement(GameObject element)
+    {
+        if (realTime)
+        {
+            element.transform.RotateAround(Vector3.zero, Vector3.right, Time.deltaTime / timeRotation);
+        }
+        else
+        {
+            element.transform.RotateAround(Vector3.zero, Vector3.right, Time.deltaTime * timeRotation);
+        }
+        element.transform.LookAt(Vector3.zero);
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+# Middleware Day/Night Cycle
+## For Unity 2017.4.1f1 or above
+#### Date: May 02, 2018
+#### Version: 1.0 alpha
+
+
+- Platform:
+	- Multiplatform
+
+- Included files/folders:
+	- Unity Assets folder
+	- Project Settings folder
+	- README.md
+	- LICENSE
+	- DayNight.gif
+
+- Versions:
+	- Unity: **2017.4.1f1**
+
+- How to init:
+	- Please import the project in Unity
+	- Double click in "DayNightScene" to test the Demo
+	- To finish click in the "Play" button of Unity and enjoy it.
+
+- How to uses:
+	- The only elements that you need is **DayNightCycle.prefab** and the **DayNightCycle.cs**, in the future I will upload this to the Asset Store, but needs more detail.
+	- In the DayNightCycle script you have four variables.
+		- Distance Sun (Is the position point max of your Sun. The default value is 500 if you not change it.)
+		- Rotation Sun (Is the sun zenith angle)
+		- Time Rotation (Is the movement of the Sun, if you check the "Real Time" is slow, otherwise if you "Real Time" is false this is fast.)
+		- Real Time (The last variable, determine the realistic movement.)
+- Github
+	- [Signeus / middleware - Day Night Cycle](https://github.com/signeus/middleware-DayNightCycle)
